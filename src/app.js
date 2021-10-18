ff0504f526bdaf44a8f5fafd44ee4964e7b9b32e
İsmail Özcan <l5215067@std.yildiz.edu.tr>
2021-10-19 00:03:28 +0300

    18Ekim JS DersÖzeti

---
+++ src/app.js
@@ -0,0 +1,35 @@
+console.log("merhaba janimm")
+
+//JS type safe değildir!!!!!
+
+let dolarDun=9.20
+//dolarDun="Ankara"
+//console.log(dolarBugun)
+let dolarBugun=9.30
+//let dolarr
+//console.log(dolarr)
+
+{
+    let dolarDun=9.10
+}
+
+console.log(dolarDun)
+
+const euroDun=11.2
+//euroDun=11
+
+console.log(euroDun)
+
+//array
+let konutKredileri=["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"]
+
+console.log("<ul>")
+for(let i=0;i<konutKredileri.length;i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")   
+    
+}
+
+console.log("</ul>")
+
+console.log(konutKredileri)
+
